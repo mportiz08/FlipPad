c26449f74c52fbe2509267671cf95f5909e1b032
Marcus Ortiz <mportiz08@gmail.com>
2010-10-29 00:42:24 -0700

    first commit

---
+++ src/com/marcusortiz/parkplace/ParkPlaceActivity.java
@@ -0,0 +1,14 @@
+package com.marcusortiz.parkplace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ParkPlaceActivity extends Activity
+{
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+}
