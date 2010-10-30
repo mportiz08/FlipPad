0c2984b347d3d24b9a61b201fb06538266022822
Marcus Ortiz <mportiz08@gmail.com>
2010-10-30 16:01:32 -0700

    added graphics

--- src/com/marcusortiz/parkplace/ParkPlaceActivity.java
+++ src/com/marcusortiz/parkplace/ParkPlaceActivity.java
@@ -2,6 +2,7 @@ package com.marcusortiz.parkplace;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class ParkPlaceActivity extends Activity
 {
@@ -9,6 +10,7 @@ public class ParkPlaceActivity extends Activity
   public void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.main);
   }
 }
