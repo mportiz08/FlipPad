bdfffa9f62c16394cbeb6ed43db7af317479bb47
Marcus Ortiz <mportiz08@gmail.com>
2010-10-31 00:37:21 -0700

    basic touch functionality working

--- src/com/marcusortiz/flippad/FlipPad.java
+++ src/com/marcusortiz/flippad/FlipPad.java
@@ -1,33 +1,36 @@
 package com.marcusortiz.flippad;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class FlipPad
 {
   public final String[] SYMBOLS = {"1", "2", "3", "4", "5", "6"};
   
-  private ArrayList<String> options;
   private int index;
+  private HashMap<String, Integer> idMap;
   
   public FlipPad()
   {
-    options = new ArrayList<String>(Arrays.asList(SYMBOLS));
     index = 0;
+    mapIds();
   }
   
-  public FlipPad(String sym)
+  public int next()
   {
-    options = new ArrayList<String>(Arrays.asList(SYMBOLS));
-    index = options.indexOf(sym);
-  }
-  
-  public String next() {   
-    String src = "@drawable/flip";
-    
-    src.concat(options.get(index % options.size()));
+    int id = idMap.get(SYMBOLS[index % SYMBOLS.length]);
     index++;
     
-    return src;
+    return id;
+  }
+  
+  private void mapIds()
+  {
+    idMap = new HashMap<String, Integer>();
+    idMap.put(SYMBOLS[0], R.drawable.flip1);
+    idMap.put(SYMBOLS[1], R.drawable.flip2);
+    idMap.put(SYMBOLS[2], R.drawable.flip3);
+    idMap.put(SYMBOLS[3], R.drawable.flip4);
+    idMap.put(SYMBOLS[4], R.drawable.flip5);
+    idMap.put(SYMBOLS[5], R.drawable.flip6);
   }
 }
--- src/com/marcusortiz/flippad/FlipPadActivity.java
+++ src/com/marcusortiz/flippad/FlipPadActivity.java
@@ -4,7 +4,10 @@ import com.marcusortiz.flippad.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
 public class FlipPadActivity extends Activity
 {
@@ -14,5 +17,34 @@ public class FlipPadActivity extends Activity
     super.onCreate(savedInstanceState);
     requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.main);
+    initPad();
+  }
+  
+  private void initPad()
+  {
+    FlipPad pad = new FlipPad();
+    ImageView padView = (ImageView)findViewById(R.id.PadView);
+    
+    padView.setImageResource(pad.next());
+    padView.setOnTouchListener(new TouchListener(pad));
+  }
+  
+  private class TouchListener implements View.OnTouchListener
+  {
+    private FlipPad pad;
+    
+    public TouchListener(FlipPad pad)
+    {
+      this.pad = pad;
+    }
+    
+    @Override
+    public boolean onTouch(View v, MotionEvent event)
+    {
+      ImageView padView = (ImageView)v;
+      padView.setImageResource(pad.next());
+      
+      return false;
+    }
   }
 }
